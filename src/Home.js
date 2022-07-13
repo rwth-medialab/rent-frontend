f4f2ba381142c0fc427847042df2b367bdab190f
Nils <github@anonymeanonymiker.de>
2022-07-13 06:00:21 +0200

    rename files make app name env

---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './Home.css';
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> ansssd save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Home;
