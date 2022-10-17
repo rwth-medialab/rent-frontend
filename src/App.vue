6bb9a6636a7ec20d5e566047e890cf3fcc3e0895
Nils <moin@heesemann.eu>
2022-10-17 20:17:22 +0200

    fix app bar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <v-app>
-    <v-app-bar dense>
-
-      <v-toolbar-title>Page title</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon to="/account">
-        <v-icon>
-          mdi-account
-        </v-icon>
-      </v-btn> |
-      <v-btn icon href="/login">
-        <v-icon>
-          mdi-login
-        </v-icon>
-      </v-btn>
-    </v-app-bar>
+    <div>
+      <v-app-bar dense dark>
+        <v-toolbar-title>
+          <v-btn href="/">Ausleihe</v-btn>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon to="/account">
+          <v-icon>
+            mdi-account
+          </v-icon>
+        </v-btn> |
+        <v-btn icon href="/login">
+          <v-icon>
+            mdi-login
+          </v-icon>
+        </v-btn>
+      </v-app-bar>
+    </div>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  data:function() {
-return {
-    loggedIn: false,
-  };
-}
+  data: function () {
+    return {
+      loggedIn: false,
+    };
+  }
 }
 </script>
