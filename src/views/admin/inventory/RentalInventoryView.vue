f17da2645342e1798821e2944282a85de4e32529
Nils <nils.heesemann@rwth-aachen.de>
2022-12-14 02:10:12 +0100

    Working Type edititing. WIP Object display

--- src/views/admin/inventory/RentalInventoryView.vue
+++ src/views/admin/inventory/RentalInventoryView.vue
@@ -14,6 +14,9 @@ export default {
       isCategoryEditDialogOpen: false,
       toBeEditedCategory: {},
       isTypeDetailsDialogOpen: false,
+      toBeEditedObjectsType: {},
+      openTypeImage: null,
+      toBeUploadedImage: null,
     };
   },
   async mounted() {
@@ -21,7 +24,63 @@ export default {
     console.log(this.categories);
   },
   methods: {
-    openTypeDetailsDialog() {},
+    openTypeDetailsDialog(type) {
+      this.isTypeDetailsDialogOpen = true;
+      this.toBeEditedObjectsType = { ...type };
+      this.toBeEditedObjectsType["image"] = this.toBeEditedObjectsType[
+        "image"
+      ].replace("http://", "https://");
+      this.openTypeImage = this.toBeEditedObjectsType["image"];
+      console.log(this.toBeEditedObjectsType);
+    },
+    saveEditedObjectsType() {
+      let formData = new FormData();
+      if (this.openTypeImage.includes("base64")) {
+        //string containes 'base64' => uploaded image
+        console.log(this.openTypeImage);
+        formData.append(this.toBeUploadedImage["type"], this.toBeUploadedImage);
+      }
+      for (const key in this.toBeEditedObjectsType) {
+        if (
+          Object.prototype.hasOwnProperty.call(
+            this.toBeEditedObjectsType,
+            key
+          ) &&
+          key != "rentalobjects"
+        ) {
+          if (key == "tags" && this.toBeEditedObjectsType[key].length < 1) {
+            continue;
+          }
+          if (
+            key == "image" &&
+            this.toBeEditedObjectsType[key].includes("http")
+          ) {
+            //image has not been changed => shouldnt be uploaded
+            continue;
+          }
+          const element = this.toBeEditedObjectsType[key];
+          formData.append(key, element);
+        }
+      }
+      console.log(...formData);
+      this.userStore.patchURLWithAuth({
+        url: "rentalobjecttypes/" + this.toBeEditedObjectsType["id"],
+        params: formData,
+      });
+      setTimeout(this.updateData, 200);
+    },
+    createEditedObjectsType() {},
+    tempSaveTypeImage(e) {
+      //temporary save the image as bas64 to display it temporarily
+      const image = e.target.files[0];
+      this.toBeUploadedImage = image;
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = (e) => {
+        this.openTypeImage = e.target.result;
+      };
+      this.toBeEditedObjectsType["image"] = this.toBeUploadedImage;
+    },
     async updateData() {
       this.categories.splice(
         0,
@@ -46,6 +105,11 @@ export default {
         });
       });
       this.objecttypes = resulttypes;
+      const objects = await this.userStore.getFromURLWithAuth({
+        url: "rentalobjects",
+      });
+      let tempObjects = {};
+      //TODO sort objects to type
       console.log(resulttypes);
       console.log(this.objecttypes);
     },
@@ -84,6 +148,11 @@ export default {
         this.toBeEditedCategory = {};
       }
     },
+    isTypeDetailsDialogOpen: function (newvalue, oldvalue) {
+      if (!newvalue) {
+        this.toBeEditedObjectsType = {};
+      }
+    },
   },
 };
 </script>
@@ -112,78 +181,147 @@ export default {
       </v-card-actions></v-card
     >
   </v-dialog>
-  <div class="d-flex flex-row mb-6">
-
-  </div>
-  <v-card
-    class="categorycard"
-    v-for="(category, index) in categories"
-    :key="category.id"
-  >
-    <v-list>
-      <v-list-group>
-        <template v-slot:activator="{ props }">
-          <v-list-item v-bind="props" :subtitle="category.description">
-            <template v-slot:title="title">
-              {{ category.name }}
-              <v-btn
-                v-if="userStore.has_inventory_rights"
-                size="small"
-                flat
-                icon="mdi-pencil"
-                @click.stop
-                @click="editCategory(index)"
-              ></v-btn>
-            </template>
-          </v-list-item>
-        </template>
-        <v-list-item v-for="rentaltype in objecttypes[categories[index]['id']]">
-          <v-list-group>
-            <template v-slot:activator="{ props }">
-              <v-list-item v-bind="props" class="type-card">
-                <div class="d-inline-flex">
-                  <v-avatar class="ma-3" size="80" rounded="0">
-                    <v-img
-                      cover
-                      aspect-ratio="1"
-                      :src="rentaltype['image']"
-                    ></v-img>
-                  </v-avatar>
-                  <div>
-                    <v-card-title class="text-xs-h5">
-                      {{ rentaltype["name"] }}
-                    </v-card-title>
-
-                    <v-card-actions>
-                      <v-btn
-                        class="ml-2"
-                        variant="outlined"
-                        size="small"
-                        @click.stop
-                        @click="action"
-                      >
-                        Details
-                      </v-btn>
-                    </v-card-actions>
-                  </div>
-                </div>
-              </v-list-item>
-            </template>
-          </v-list-group></v-list-item
+  <v-dialog v-model="isTypeDetailsDialogOpen">
+    <v-card class="pa-3">
+      <h2 class="ma-2">Details von: {{ toBeEditedObjectsType["name"] }}</h2>
+      <v-text-field
+        label="Name"
+        v-model="toBeEditedObjectsType['name']"
+        :readonly="!userStore.has_inventory_rights()"
+      ></v-text-field>
+      <v-textarea
+        label="Beschreibung"
+        v-model="toBeEditedObjectsType['description']"
+        :readonly="!userStore.has_inventory_rights()"
+      ></v-textarea>
+      <v-text-field
+        label="Präfix für ID"
+        v-model="toBeEditedObjectsType['prefix_identifier']"
+        :readonly="!userStore.has_inventory_rights()"
+      ></v-text-field>
+      <v-checkbox
+        label="In Verleihübersicht anzeigen"
+        :readonly="!userStore.has_inventory_rights()"
+        v-model="toBeEditedObjectsType['visible']"
+      ></v-checkbox>
+      <v-row>
+        <v-avatar class="ma-3" size="100" rounded="0">
+          <v-img cover aspect-ratio="1" :src="openTypeImage"></v-img>
+          <!-- TODO replace "replace(http://, https://)" with something better, maybe django could send the correct protocol -->
+          <!-- Suggestions between types-->
+        </v-avatar>
+        <v-col>
+          <v-file-input
+            class="mr-3"
+            density="compact"
+            accept="image/*"
+            @change="tempSaveTypeImage"
+            v-if="userStore.has_inventory_rights"
+          ></v-file-input>
+        </v-col>
+      </v-row>
+      <v-card-actions>
+        <v-spacer></v-spacer
+        ><v-btn @click="isTypeDetailsDialogOpen = false">Abbrechen</v-btn>
+        <v-btn v-if="toBeEditedObjectsType['id']" @click="saveEditedObjectsType"
+          >Speichern</v-btn
         >
-        <v-list-item>
-          <v-btn
-            elevation="0"
-            prepend-icon="mdi-plus"
-            action
-            @click.stop
-            @click="isTypeDetailsDialogOpen = true"
-            >Neuen Typ hinzufügen</v-btn
-          >
-        </v-list-item>
-      </v-list-group>
-    </v-list>
-  </v-card>
+        <v-btn v-else @click="createEditedObjectsType">Erstellen</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+  <div class="d-flex flex-wrap">
+    <v-card
+      class="categorycard w-100"
+      v-for="(category, index) in categories"
+      :key="category.id"
+    >
+      <v-list>
+        <v-list-group>
+          <template v-slot:activator="{ props }">
+            <v-list-item v-bind="props" :subtitle="category.description">
+              <template v-slot:title="title">
+                {{ category.name }}
+                <v-btn
+                  v-if="userStore.has_inventory_rights"
+                  size="small"
+                  flat
+                  icon="mdi-pencil"
+                  @click.stop
+                  @click="editCategory(index)"
+                ></v-btn>
+              </template>
+            </v-list-item>
+          </template>
+          <v-row class="ma-1">
+            <v-list-item
+              :class="{
+                'w-30': !$vuetify.display.mobile,
+                'w-100': $vuetify.display.mobile,
+              }"
+              v-for="rentaltype in objecttypes[categories[index]['id']]"
+            >
+              <v-list-group>
+                <template v-slot:activator="{ props }">
+                  <v-list-item v-bind="props" class="type-card">
+                    <div class="d-inline-flex">
+                      <v-avatar class="ma-3" size="80" rounded="0">
+                        <v-img
+                          cover
+                          aspect-ratio="1"
+                          :src="
+                            rentaltype['image'].replace('http://', 'https://')
+                          "
+                        ></v-img>
+                        <!-- TODO replace "replace(http://, https://)" with something better -->
+                      </v-avatar>
+                      <div class="flex-column">
+                        <v-card-title class="text-xs-h5 d-flex">
+                          {{ rentaltype["name"] }}
+                        </v-card-title>
+
+                        <v-card-subtitle class="d-flex">
+                          <v-chip
+                            v-if="!rentaltype['visible']"
+                            class=""
+                            color="red"
+                            >versteckt</v-chip
+                          >
+                          <v-chip v-else color="green">angezeigt</v-chip>
+                        </v-card-subtitle>
+
+                        <v-card-actions>
+                          <v-btn
+                            class="ml-2"
+                            variant="outlined"
+                            size="small"
+                            @click.stop
+                            @click="openTypeDetailsDialog(rentaltype)"
+                          >
+                            Details
+                          </v-btn>
+                        </v-card-actions>
+                      </div>
+                    </div>
+                  </v-list-item>
+                </template>
+              </v-list-group></v-list-item
+            >
+          </v-row>
+          <v-list-item>
+            <v-btn
+              elevation="0"
+              prepend-icon="mdi-plus"
+              action
+              @click.stop
+              @click="isTypeDetailsDialogOpen = true"
+              >Neuen Typ hinzufügen</v-btn
+            >
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-card>
+  </div>
   <v-card class="categorycard" v-if="userStore.has_inventory_rights">
     <v-list>
       <v-list-item>
@@ -210,5 +348,17 @@ export default {
 }
 .type-card {
   padding: 0 !important;
+}
+
+.w-30 {
+  width: 30% !important;
+}
+
+.w-70 {
+  width: 60% !important;
+}
+
+.titlerow {
+  width: inherit;
 }
 </style>
