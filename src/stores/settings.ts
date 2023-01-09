7471f714349058d6cac0e8c260fdfe44c6160add
Nils <nils.heesemann@rwth-aachen.de>
2023-01-09 16:43:10 +0100

    remove settings store

--- src/stores/settings.ts
+++
@@ -1,8 +0,0 @@
-import { defineStore } from "pinia";
-import { useStorage } from "@vueuse/core";
-
-export const useSettingsStore = defineStore("settings", {
-  state: () => ({
-    theme: useStorage("settings", "dark"),
-  }),
-});
